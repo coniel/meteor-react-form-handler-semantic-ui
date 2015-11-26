5f899378c740c9b78e1595cdd77c4129600b3e0f
Oscar Coniel <oscar@coniel.com>
2015-11-26 13:55:26 +0200

    SubmitButton prop inheritance

--- lib/components/SubmitButton.jsx
+++ lib/components/SubmitButton.jsx
@@ -1,13 +1,18 @@
 SubmitButton = React.createClass({
     render: function() {
         let label = this.props.label;
+		let className = "ui button";
+		
+		if (this.props.className) {
+			className += " " + this.props.className;
+		}
 		
 		if (FormHandler.i18n) {
 			label = TAPi18n.__(label);
 		}
 
         return (
-            <button className="ui button" type="submit">{label}</button>
+            <button {...this.props} className={className} type="submit">{label}</button>
         )
     }
 });
